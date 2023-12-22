72c4e85a858061e079ac983d3cf6d69f6b65d2f7
Kamil Tarka <kamiltarka5@gmail.com>
2023-12-22 14:18:20 +0100

    basic app properties tests

--- src/main/java/pl/ktarka/students/StudentsApplication.java
+++ src/main/java/pl/ktarka/students/StudentsApplication.java
@@ -1,13 +1,14 @@
 package pl.ktarka.students;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StudentsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StudentsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsApplication.class, args);
+    }
 
 }
